b47ad34a0e098020c480a2761f38d5f7740e39f3
Cesar.Sotnas <devcarlos.cesar@gmail.com>
2024-08-28 12:07:04 -0300

    Fundamentos de GO

--- 1 - pacotes/main.go
+++ 1 - pacotes/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
 	auxiliar.Escrever2()
+
+	erro := checkmail.ValidateFormat("devcarlos.cesar1231")
+	fmt.Println((erro))
 }
